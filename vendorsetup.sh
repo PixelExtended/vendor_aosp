87087789454658827b32ada79e0750778c7ea631
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-09 22:23:56 -0300

    vendor: Add others targets on lunch menu [2/2]
    Change-Id: Ic9cfc1c11d011c09718818921475b685340d9a1c
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 lunch_others_targets=()
-for device in $(python vendor/aosp/tools/get_official_devices.py)
+for device in $(cat vendor/aosp/pex.devices)
 do
-    for var in user userdebug eng; do
+    for var in eng user userdebug; do
         lunch_others_targets+=("aosp_$device-$var")
     done
 done
