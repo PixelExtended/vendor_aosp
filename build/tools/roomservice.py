e1b01f77b5c340b5eb04dcbeabc5e67e836419eb
Aryan Gupta <guptaaryan189@gmail.com>
2020-10-09 00:07:08 +0530

    adapt roomservice for pixel extended

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -43,10 +43,10 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/pixel.xml"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'ten-plus')
+custom_default_revision = "r"
 custom_dependencies = "aosp.dependencies"
 org_manifest = "pixel-devices"  # leave empty if org is provided in manifest
-org_display = "PixelExperience-Devices"  # needed for displaying
+org_display = "PixelExtended-Devices"  # needed for displaying
 
 github_auth = None
 
