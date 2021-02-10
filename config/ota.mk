b323d67ca4b05f818b55d8940888beae0e1096a4
Aryan Gupta <guptaaryan189@gmail.com>
2021-02-10 10:56:35 +0000

    OTA: Build it

--- config/ota.mk
+++ config/ota.mk
@@ -1,4 +1,11 @@
 ifneq ($(filter OFFICIAL CI,$(PEX_BUILD_TYPE)),)
+
+PEX_OTA_VERSION_CODE := eleven
+
+PRODUCT_GENERIC_PROPERTIES += \
+    org.pex.ota.version_code=$(PEX_OTA_VERSION_CODE) \
+    sys.ota.disable_uncrypt=1
+
 PRODUCT_PACKAGES += \
     Updates
 endif
