d2192214e6e737389cdd285df7214bce687f2385
Aryan Gupta <guptaaryan189@gmail.com>
2021-05-29 19:26:01 +0530

    refactor build script
    export ccache and fix product call

--- tools/build.sh
+++ tools/build.sh
@@ -9,7 +9,9 @@ if [ $# -lt 2 ]; then
     echo "Missing mandatory parameters it must be like this :- bash build.sh jasmine_sprout userdebug/eng/user"
     exit 1
 fi
-
-lunch legion_$DEVICE-$TYPE
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 100G
+lunch aosp_$DEVICE-$TYPE
 make installclean
-mka bacon
+mka bacon -j$(nproc --all)
