8e0f11204d8b0ab78d51c2b962f4f7799f270eb5
Bharath <bharath@teamb58.org>
2022-01-23 23:19:11 +0900

    fonts: Import more font overlays
    * Imported from https://github.com/Project-Xtended/vendor_xtras/tree/xs/fonts
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Yash Pal <officialyash007@gmail.com>
    Co-authored-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: ashwatthama <sai404142@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Change-Id: I682188d53a4dc2d496ee46efe3a9fc9af74da3cc

--- config/fonts.mk
+++ config/fonts.mk
@@ -55,5 +55,68 @@ PRODUCT_PACKAGES += \
     Barlow-Bold.ttf \
     Barlow-Medium.ttf
 
+# Other new fonts
+PRODUCT_PACKAGES += \
+    Aclonica.ttf \
+    Amarante.ttf \
+    AntipastoPro.ttf \
+    Bariol-Regular.ttf \
+    Cagliostro-Regular.ttf \
+    Cocon.ttf \
+    Comfortaa.ttf \
+    Comic_Sans.ttf \
+    Coolstory-Regular.ttf \
+    EvolveSans.ttf \
+    Exotwo.ttf \
+    Fifa_2018.ttf \
+    Fucek.ttf \
+    GrandHotel.ttf \
+    LGSmartGothic.ttf \
+    LemonMilk.ttf \
+    Nokia_Pure.ttf \
+    Nunito-Bold.ttf \
+    Nunito-Regular.ttf \
+    Oduda.ttf \
+    Quando.ttf \
+    Redressed.ttf \
+    ReemKufi.ttf \
+    Rosemary-Regular.ttf \
+    SamsungOne.ttf \
+    SimpleDay.ttf \
+    SonySketch.ttf \
+    Storopia.ttf \
+    Surfer.ttf \
+    Ubuntu.ttf \
+    FontAclonicaSourceOverlay \
+    FontAmaranteSourceOverlay \
+    FontAntipastoProSourceOverlay \
+    FontBariolSourceOverlay \
+    FontCagliostroSourceOverlay \
+    FontCoconSourceOverlay \
+    FontComfortaSourceOverlay \
+    FontComicSansSourceOverlay \
+    FontCoolstorySourceOverlay \
+    FontEvolveSansSourceOverlay \
+    FontExotwoSourceOverlay \
+    FontFifa2018SourceOverlay \
+    FontFucekSourceOverlay \
+    FontGrandHotelSourceOverlay \
+    FontLemonMilkSourceOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontNokiaPureSourceOverlay \
+    FontNunitoSourceOverlay \
+    FontOdudaSourceOverlay \
+    FontQuandoSourceOverlay \
+    FontRedressedSourceOverlay \
+    FontReemKufiSourceOverlay \
+    FontRosemarySourceOverlay \
+    FontSamsungOneSourceOverlay \
+    FontSimpleDaySourceOverlay \
+    FontSonySketchSourceOverlay \
+    FontStoropiaSourceOverlay \
+    FontSurferSourceOverlay \
+    FontUbuntuSourceOverlay
+
+
 PRODUCT_COPY_FILES += \
     vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
