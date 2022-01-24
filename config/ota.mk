cdec26928ab08373edcd833f280049ab0c66ef2c
darkbeast69 <guptaaryan189@gmail.com>
2022-01-24 10:04:24 +0000

    Enable OTA

--- config/ota.mk
+++ config/ota.mk
@@ -1,4 +1,9 @@
-ifneq ($(filter OFFICIAL CI,$(PEX_BUILD_TYPE)),)
+ifeq ($(PEX_BUILD_TYPE), OFFICIAL)
+PEX_OTA_VERSION_CODE := twelve
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    org.pex.ota.version_code=$(PEX_OTA_VERSION_CODE)
+
 PRODUCT_PACKAGES += \
     Updates
 endif
