aebd5f0f8fa73ac8155d0cbf6b898501615576e0
Pranav Vashi <neobuddy89@gmail.com>
2022-11-21 20:55:13 +0100

    Use separate packages for UDFPS icons and animations [3/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -238,8 +238,12 @@ $(call inherit-product, vendor/overlays/overlays.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
-# UDFPS Animations
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    UdfpsIcons
+
 ifeq ($(EXTRA_UDFPS_ANIMATIONS),true)
 PRODUCT_PACKAGES += \
-    UdfpsResources
+    UdfpsAnimations
+endif
 endif
