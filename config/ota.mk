6c2a350d4524728cf7f99929c9d98586be418ba3
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-11-28 19:19:40 +0530

    vendor: Update OTA props

--- config/ota.mk
+++ config/ota.mk
@@ -1,8 +1,8 @@
 ifeq ($(PEX_BUILD_TYPE), OFFICIAL)
-PEX_OTA_VERSION_CODE := twelve
+PEX_OTA_VERSION_CODE := thirteen
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    org.pex.ota.version_code=$(PEX_OTA_VERSION_CODE)
+    org.pixelexperience.ota.version_code=$(PEX_OTA_VERSION_CODE)
 
 PRODUCT_PACKAGES += \
     Updates
