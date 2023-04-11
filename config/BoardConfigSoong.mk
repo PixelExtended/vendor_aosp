55fdadbb717b5804109b986fe779303146d17a88
inferno0230 <adityachoudhury0230@gmail.com>
2023-04-12 01:00:31 +0530

    soong: Add TARGET_NEEDS_OPLUS_VENDOR_TAG
    Change-Id: Ica0675e3b812e32459d82ea3d648829a73f26be5

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -41,7 +41,8 @@ SOONG_CONFIG_customGlobalVars += \
     uses_egl_display_array \
     uses_oplus_camera \
     uses_oppo_camera \
-    uses_nothing_camera
+    uses_nothing_camera \
+    needs_oplus_tag
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
 SOONG_CONFIG_customNvidiaVars += \
@@ -67,6 +68,7 @@ SOONG_CONFIG_customGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGE
 SOONG_CONFIG_customGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_customGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_customGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_customGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
